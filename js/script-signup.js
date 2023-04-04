62192da7890cfed67daedfbbbdebf65148790fba
yashhhhh1 <yash8511132161@gmail.com>
2023-04-04 11:17:21 +0530

    add

---
+++ js/script-signup.js
@@ -0,0 +1,65 @@
+const form = document.querySelector("form");
+nField = form.querySelector(".name"),
+nInput = nField.querySelector("input"),
+eField = form.querySelector(".email"),
+eInput = eField.querySelector("input"),
+pField = form.querySelector(".password"),
+pInput = pField.querySelector("input");
+
+form.onsubmit = (e)=>{
+  e.preventDefault(); //preventing from form submitting
+  //if name and email and password is blank then add shake class in it else call specified function
+  (nInput.value == "") ? nField.classList.add("shake", "error") : checkName();
+  (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
+  (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
+
+  setTimeout(()=>{ //remove shake class after 500ms
+    nField.classList.remove("shake");
+    eField.classList.remove("shake");
+    pField.classList.remove("shake");
+  }, 500);
+
+  nInput.onkeyup = ()=>{checkName();} //calling checkName function on name input keyup
+  eInput.onkeyup = ()=>{checkEmail();} //calling checkEmail function on email input keyup
+  pInput.onkeyup = ()=>{checkPass();} //calling checkPassword function on pass input keyup
+
+
+  function checkName() {
+    if(nInput.value == ""){ //if name is empty then add error and remove valid class
+      nField.classList.add("error");
+      nField.classList.remove("valid");
+    }else{ //if name is empty then remove error and add valid class
+      nField.classList.remove("error");
+      nField.classList.add("valid");
+    }
+  }
+
+  function checkEmail(){ //checkEmail function
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern for validate email
+    if(!eInput.value.match(pattern)){ //if pattern not matched then add error and remove valid class
+      eField.classList.add("error");
+      eField.classList.remove("valid");
+      let errorTxt = eField.querySelector(".error-txt");
+      //if email value is not empty then show please enter valid email else show Email can't be blank
+      (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address" : errorTxt.innerText = "Email can't be blank";
+    }else{ //if pattern matched then remove error and add valid class
+      eField.classList.remove("error");
+      eField.classList.add("valid");
+    }
+  }
+
+  function checkPass(){ //checkPass function
+    if(pInput.value == ""){ //if pass is empty then add error and remove valid class
+      pField.classList.add("error");
+      pField.classList.remove("valid");
+    }else{ //if pass is empty then remove error and add valid class
+      pField.classList.remove("error");
+      pField.classList.add("valid");
+    }
+  }
+
+  //if eField and pField doesn't contains error class that mean user filled details properly
+  if( !nField.classList.contains("error") && !eField.classList.contains("error") && !pField.classList.contains("error")){
+    window.location.href = form.getAttribute("action"); //redirecting user to the specified url which is inside action attribute of form tag
+  }
+}
